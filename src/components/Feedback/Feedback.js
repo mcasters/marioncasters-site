12953aaff6bd4690d00740075923e5ccbc8e8cae
Unknown <yonk@wanadoo.fr>
2018-10-14 21:38:09 +0200

    tools logo adds

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -3,14 +3,31 @@ import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Feedback.css';
 import Link from '../Link';
+import githubLogo from './githubTransparent.png';
+import graphqlLogo from './graphqlTransparent.png';
+import nodejsLogo from './nodeJSTransparent.png';
+import reactLogo from './reactTransparent.png';
+import appleLogo from './appleTransparent.png';
 
 class Feedback extends React.Component {
   render() {
     return (
       <aside>
         <div className={s.container}>
-          <Link className={s.link} to="/contact">
-            Contact
+          <Link to="/">
+            <img src={nodejsLogo} className={s.nodejsLogo} alt="NodeJS" />
+          </Link>
+          <Link to="/">
+            <img src={reactLogo} className={s.reactLogo} alt="React" />
+          </Link>
+          <Link to="/">
+            <img src={graphqlLogo} className={s.graphqlLogo} alt="GraphQL" />
+          </Link>
+          <Link to="/">
+            <img src={githubLogo} className={s.githubLogo} alt="GitHub" />
+          </Link>
+          <Link to="/">
+            <img src={appleLogo} className={s.appleLogo} alt="GitHub" />
           </Link>
         </div>
       </aside>
