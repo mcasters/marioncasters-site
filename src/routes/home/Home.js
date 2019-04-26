f778fe71fcbe54dba2044a4d14f5f0eb4363c37c
Unknown <yonk@wanadoo.fr>
2019-04-26 23:47:50 +0200

    Start home page

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import withStyles from 'isomorphic-style-loader/withStyles';
+
+import s from './Home.css';
+import Content from '../../components/Content';
+import contentConstnats from '../../constants/contentConstants';
 
 class Home extends React.Component {
   static propTypes = {
@@ -7,13 +12,23 @@ class Home extends React.Component {
   };
 
   render() {
+    const CONTENT_CONSTANTS = contentConstnats;
     const { title } = this.props;
     return (
       <div>
         <h1>{title}</h1>
+        <div className={s.content}>
+          <Content keyContent={CONTENT_CONSTANTS.KEY.HOME1} />
+        </div>
+        <div className={s.content}>
+          <Content keyContent={CONTENT_CONSTANTS.KEY.HOME2} />
+        </div>
+        <div className={s.content}>
+          <Content keyContent={CONTENT_CONSTANTS.KEY.HOME3} />
+        </div>
       </div>
     );
   }
 }
 
-export default Home;
+export default withStyles(s)(Home);
