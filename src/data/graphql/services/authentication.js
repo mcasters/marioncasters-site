331abfa3bbf718e2a80f43db19f493dfca8e7597
Unknown <yonk@wanadoo.fr>
2019-05-13 17:37:15 +0200

    Fix authentication issue

--- src/data/graphql/services/authentication.js
+++ src/data/graphql/services/authentication.js
@@ -3,10 +3,11 @@ import { User } from '../../models';
 const getAuthenticatedUser = async req => {
   if (!req.session.userId) return false;
 
-  // eslint-disable-next-line no-return-await
-  return await User.findOne({
+  const user = await User.findOne({
     where: { id: req.session.userId },
   });
+
+  return !!user;
 };
 
 export default getAuthenticatedUser;
