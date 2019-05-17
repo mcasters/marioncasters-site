ab0c4dea0dfbdd536b998bf638bc312085672f0d
Unknown <yonk@wanadoo.fr>
2019-05-17 18:16:26 +0200

    Correction in relation to chrome audit

--- src/components/GeneralLayout/Feedback/Feedback.js
+++ src/components/GeneralLayout/Feedback/Feedback.js
@@ -11,16 +11,16 @@ class Feedback extends React.Component {
   render() {
     return (
       <div className={s.container}>
-        <Link to="https://nodejs.org" target="_blank">
+        <Link to="https://nodejs.org" target="_blank" rel="noreferrer">
           <img src={nodejsLogo} className={s.nodejsLogo} alt="NodeJS" />
         </Link>
-        <Link to="https://reactjs.org" target="_blank">
+        <Link to="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className={s.reactLogo} alt="React" />
         </Link>
-        <Link to="https://graphql.org" target="_blank">
+        <Link to="https://graphql.org" target="_blank" rel="noreferrer">
           <img src={graphqlLogo} className={s.graphqlLogo} alt="GraphQL" />
         </Link>
-        <Link to="https://www.apple.com" target="_blank">
+        <Link to="https://www.apple.com" target="_blank" rel="noreferrer">
           <img src={appleLogo} className={s.appleLogo} alt="Apple" />
         </Link>
       </div>
