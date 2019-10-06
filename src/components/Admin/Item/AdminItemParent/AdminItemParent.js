abc73329c46addcfc46e7dca1ec7f46e8495a802
Unknown <yonk@wanadoo.fr>
2019-10-06 14:10:51 +0200

    Add Item mutation

--- src/components/Admin/Item/AdminItemParent/AdminItemParent.js
+++ src/components/Admin/Item/AdminItemParent/AdminItemParent.js
@@ -1,24 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useMutation } from 'react-apollo';
 
 import ItemList from '../ItemList';
-import ItemAdd from '../ItemAdd';
+import ItemAddForm from '../ItemAdd';
+import ADD_ITEM_MUTATION from '../../../../data/graphql/queries/addItemMutation.graphql';
+import GET_ITEMS_QUERY from '../../../../data/graphql/queries/getAllItems.graphql';
 
-class AdminItemParent extends React.Component {
-  render() {
-    const { type } = this.props;
+export default function AdminItemParent({ type }) {
+  const [addItem, { loading, error }] = useMutation(ADD_ITEM_MUTATION, {
+    update(cache, { data }) {
+      const { getAllItems } = cache.readQuery({
+        query: GET_ITEMS_QUERY,
+        variables: {
+          type,
+        },
+      });
+      cache.writeQuery({
+        query: GET_ITEMS_QUERY,
+        variables: {
+          type,
+        },
+        data: { getAllItems: [...getAllItems, data.addItem] },
+      });
+    },
+  });
 
-    return (
-      <>
-        <ItemAdd type={type} />
-        <ItemList type={type} />
-      </>
-    );
-  }
+  if (loading) return <p>OK</p>;
+  if (error) return <p>An error occurred</p>;
+
+  return (
+    <>
+      <ItemAddForm type={type} mutation={addItem} />
+      <ItemList type={type} />
+    </>
+  );
 }
 
 AdminItemParent.propTypes = {
   type: PropTypes.string.isRequired,
 };
-
-export default AdminItemParent;
