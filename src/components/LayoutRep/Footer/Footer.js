0f7025bad6facbffe440eb79feada03489efdf77
Unknown <yonk@wanadoo.fr>
2019-10-06 16:40:11 +0200

    Login query

--- src/components/LayoutRep/Footer/Footer.js
+++ src/components/LayoutRep/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/withStyles';
+import { useQuery } from 'react-apollo';
 
 import s from './Footer.css';
 import Link from '../../Link';
@@ -7,28 +8,32 @@ import LoginControl from '../../LoginControl';
 import GLOBAL_CONSTANTS from '../../../constants/globalConstants';
 import Feedback from '../Feedback';
 import ROUTER_CONSTANTS from '../../../constants/routerConstants';
+import GET_ADMIN_STATUS_QUERY from '../../../data/graphql/queries/getAdminStatusQuery.graphql';
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer>
-        <Feedback />
-        <div className={s.container}>
-          <span className={s.text}>{GLOBAL_CONSTANTS.COPYRIGHT}</span>
-          <span className={s.dot}>·</span>
-          <Link className={s.homeLink} to={ROUTER_CONSTANTS.ROUTER.HOME}>
-            Home
-          </Link>
-          <span className={s.dot}>·</span>
-          <LoginControl />
-          <span className={s.dot}>·</span>
-          <Link className={s.link} to={ROUTER_CONSTANTS.ROUTER.CONFIDENTIALITE}>
-            Privacy
-          </Link>
-        </div>
-      </footer>
-    );
-  }
+function Footer() {
+  const { data } = useQuery(GET_ADMIN_STATUS_QUERY, {
+    ssr: true,
+  });
+
+  const isConnected = data !== undefined && data.adminStatus.isConnected;
+  return (
+    <footer>
+      <Feedback />
+      <div className={s.container}>
+        <span className={s.text}>{GLOBAL_CONSTANTS.COPYRIGHT}</span>
+        <span className={s.dot}>·</span>
+        <Link className={s.homeLink} to={ROUTER_CONSTANTS.ROUTER.HOME}>
+          Home
+        </Link>
+        <span className={s.dot}>·</span>
+        <LoginControl isConnected={isConnected} />
+        <span className={s.dot}>·</span>
+        <Link className={s.link} to={ROUTER_CONSTANTS.ROUTER.CONFIDENTIALITE}>
+          Privacy
+        </Link>
+      </div>
+    </footer>
+  );
 }
 
 export default withStyles(s)(Footer);
--- src/components/LoginControl/LoginControl.js
+++ src/components/LoginControl/LoginControl.js
@@ -1,63 +1,59 @@
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/withStyles';
-import { Query } from 'react-apollo';
+import PropTypes from 'prop-types';
 
 import s from './LoginControl.css';
 import Link from '../Link';
 import LoginDialog from '../LoginDialog';
-import GET_ADMIN_STATUS_QUERY from '../../data/graphql/queries/getAdminStatusQuery.graphql';
 
 class LoginControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      login: false,
+      openLogin: false,
     };
 
     this.openLogin = this.openLogin.bind(this);
   }
 
   componentWillUnmount = () => {
-    this.setState({ login: false });
+    this.setState({ openLogin: false });
   };
 
   openLogin = e => {
     e.preventDefault();
-    this.setState({ login: true });
+    this.setState({ openLogin: true });
   };
 
   closeLogin = () => {
-    this.setState({ login: false });
+    this.setState({ openLogin: false });
   };
 
   render() {
-    return (
-      <Query query={GET_ADMIN_STATUS_QUERY}>
-        {({ data: adminStatus }) => {
-          return adminStatus !== null &&
-            adminStatus !== undefined &&
-            adminStatus.isConnected ? (
-            <Link className={s.link} to="/admin">
-              Admin
-            </Link>
-          ) : (
-            <>
-              <button
-                type="button"
-                className={s.loginLink}
-                onClick={e => {
-                  this.openLogin(e);
-                }}
-              >
-                Admin in
-              </button>
-              {this.state.login && <LoginDialog onClose={this.closeLogin} />}
-            </>
-          );
-        }}
-      </Query>
+    return this.props.isConnected ? (
+      <Link className={s.link} to="/admin">
+        Admin
+      </Link>
+    ) : (
+      <>
+        <button
+          type="button"
+          className={s.loginLink}
+          onClick={e => {
+            e.preventDefault();
+            this.openLogin(e);
+          }}
+        >
+          Admin in
+        </button>
+        {this.state.openLogin && <LoginDialog onClose={this.closeLogin} />}
+      </>
     );
   }
 }
+
+LoginControl.propTypes = {
+  isConnected: PropTypes.bool.isRequired,
+};
 
 export default withStyles(s)(LoginControl);
