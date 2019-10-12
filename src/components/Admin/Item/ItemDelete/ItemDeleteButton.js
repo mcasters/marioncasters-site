ec342ad80d9fab937a5d06575fad23f66ea3b3d0
Unknown <yonk@wanadoo.fr>
2019-10-12 12:51:25 +0200

    Fix Fa import too heavy

--- src/components/Admin/Item/ItemDelete/ItemDeleteButton.js
+++ src/components/Admin/Item/ItemDelete/ItemDeleteButton.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FaTrash } from 'react-icons/fa/index';
+import { FaTrash } from 'react-icons/fa';
 import withStyles from 'isomorphic-style-loader/withStyles';
 import s from './ItemDeleteButton.css';
 
