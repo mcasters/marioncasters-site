a630e6b34edf05844007eca718a00df5c3219bb6
Unknown <yonk@wanadoo.fr>
2019-10-18 01:20:59 +0200

    Toggle addItemForm component to functional

--- src/components/Admin/Item/ItemAdd/ItemAddForm.js
+++ src/components/Admin/Item/ItemAdd/ItemAddForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/withStyles';
 import DayPickerInput from 'react-day-picker/DayPickerInput';
@@ -7,200 +7,200 @@ import { formatDate, parseDate } from 'react-day-picker/moment';
 import s from './ItemAddForm.css';
 import ITEM_CONST from '../../../../constants/itemConstants';
 
-class ItemAddForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.isSculpture = this.props.type === ITEM_CONST.SCULPTURE.TYPE;
-    this.state = this.getInitialState();
+const initialState = {
+  title: '',
+  date: '',
+  technique: '',
+  description: '',
+  length: '',
+  height: '',
+  width: '',
+  pictures: [],
+};
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleDayChange = this.handleDayChange.bind(this);
-    this.handleImageChange = this.handleImageChange.bind(this);
-    this.constructItem = this.constructItem.bind(this);
-  }
+function ItemAddForm({ type, addMutation }) {
+  const [
+    { title, date, technique, description, length, height, width, pictures },
+    setState,
+  ] = useState(initialState);
+  const [imagePreviewUrls, setImagePreviewUrls] = useState([]);
 
-  getInitialState = () => ({
-    title: '',
-    date: '',
-    technique: '',
-    description: '',
-    length: '',
-    height: '',
-    width: '',
-    imagePreviewUrls: [],
-    pictures: [],
-  });
+  const isSculpture = type === ITEM_CONST.SCULPTURE.TYPE;
 
-  constructItem = () => {
-    const { imagePreviewUrls, length, ...rest } = this.state;
-    return !this.isSculpture
-      ? {
-          ...rest,
-          type: this.props.type,
-        }
-      : {
-          ...rest,
-          length: this.state.length,
-          type: this.props.type,
-        };
+  const clearState = () => {
+    setState({ ...initialState });
+    setImagePreviewUrls([]);
   };
 
-  handleImageChange(e, index) {
+  const onChange = e => {
+    if (e.target === undefined)
+      setState(prevState => ({ ...prevState, date: e }));
+    else {
+      // eslint-disable-next-line no-shadow
+      const { name, value, type } = e.target;
+      setState(prevState => ({
+        ...prevState,
+        [name]: type === 'number' ? parseInt(value, 10) : value,
+      }));
+    }
+  };
+
+  const onImageChange = (e, index) => {
     e.preventDefault();
 
     const reader = new FileReader();
     const file = e.target.files[0];
 
-    const { imagePreviewUrls, pictures } = this.state;
+    const pImagePreviewUrls = imagePreviewUrls;
+    const pPictures = pictures;
     reader.onload = () => {
-      imagePreviewUrls.splice(index, 1, reader.result);
-      pictures.splice(index, 1, file);
+      pImagePreviewUrls.splice(index, 1, reader.result);
+      pPictures.splice(index, 1, file);
 
-      this.setState({ imagePreviewUrls });
-      this.setState({ pictures });
+      setImagePreviewUrls(pImagePreviewUrls);
+      setState(prevState => ({ ...prevState, pictures: pPictures }));
     };
     reader.readAsDataURL(file);
+  };
+
+  // parseInt(value, 10)
+  const titleForm = 'Ajout';
+  const haveMain = !!(title && date && technique && height && width);
+  const canSubmit =
+    (!isSculpture && haveMain && pictures.length === 1) ||
+    (isSculpture && haveMain && length && pictures.length === 4);
+
+  function buildInput() {
+    const input = {
+      title,
+      date,
+      technique,
+      description,
+      height,
+      width,
+      pictures,
+      type,
+    };
+    return isSculpture
+      ? {
+          ...input,
+          length,
+        }
+      : input;
   }
 
-  handleInputChange(e) {
-    e.preventDefault();
-    const { value, name, type } = e.target;
-    this.setState({ [name]: type === 'number' ? parseInt(value, 10) : value });
-  }
-
-  handleDayChange(selectedDay) {
-    this.setState({ date: selectedDay });
-  }
-
-  render() {
-    const title = 'Ajout';
-    const haveMain = !!(
-      this.state.title &&
-      this.state.date &&
-      this.state.technique &&
-      this.state.height &&
-      this.state.width
-    );
-    const canSubmit =
-      (!this.isSculpture && haveMain && this.state.pictures.length === 1) ||
-      (this.isSculpture &&
-        haveMain &&
-        this.state.length &&
-        this.state.pictures.length === 4);
-
-    return (
-      <div className={s.addContainer}>
-        <h2>{title}</h2>
-        <form
-          className="formGroup"
-          onSubmit={e => {
-            e.preventDefault();
-            const input = this.constructItem();
-            this.props.addMutation({ variables: { input } }).then(res => {
-              if (res) this.setState(this.getInitialState);
-            });
-          }}
-        >
+  return (
+    <div className={s.addContainer}>
+      <h2>{titleForm}</h2>
+      <form
+        className="formGroup"
+        onSubmit={e => {
+          e.preventDefault();
+          const input = buildInput();
+          addMutation({ variables: { input } }).then(res => {
+            if (res) clearState();
+          });
+        }}
+      >
+        <input
+          className={s.inputL}
+          placeholder="Titre"
+          name="title"
+          type="text"
+          value={title}
+          onChange={onChange}
+        />
+        <div className={s.DayInputContainer}>
+          <DayPickerInput
+            value={date}
+            onDayChange={onChange}
+            formatDate={formatDate}
+            parseDate={parseDate}
+            format={ITEM_CONST.FORMAT_DATE}
+            placeholder="Date"
+          />
+        </div>
+        <input
+          className={s.inputL}
+          placeholder="Technique"
+          name="technique"
+          type="text"
+          value={technique}
+          onChange={onChange}
+        />
+        <input
+          className={s.inputR}
+          placeholder="Description"
+          name="description"
+          type="text"
+          value={description}
+          onChange={onChange}
+        />
+        <input
+          className={s.inputL}
+          placeholder="Hauteur (cm)"
+          name="height"
+          type="number"
+          value={height}
+          onChange={onChange}
+        />
+        <input
+          className={s.inputR}
+          placeholder="Largeur (cm)"
+          name="width"
+          type="number"
+          value={width}
+          onChange={onChange}
+        />
+        {isSculpture && (
           <input
             className={s.inputL}
-            placeholder="Titre"
-            name="title"
-            type="text"
-            value={this.state.title}
-            onChange={this.handleInputChange}
+            placeholder="Longueur (cm)"
+            name="length"
+            type="number"
+            value={length}
+            onChange={onChange}
           />
-          <div className={s.DayInputContainer}>
-            <DayPickerInput
-              value={this.state.date}
-              onDayChange={this.handleDayChange}
-              formatDate={formatDate}
-              parseDate={parseDate}
-              format={ITEM_CONST.FORMAT_DATE}
-              placeholder="Date"
+        )}
+        <input
+          type="file"
+          accept="image/jpeg, image/jpg"
+          onChange={e => onImageChange(e, 0)}
+        />
+        {isSculpture && (
+          <div>
+            <input
+              type="file"
+              accept="image/jpeg, image/jpg"
+              onChange={e => onImageChange(e, 1)}
+            />
+            <input
+              type="file"
+              accept="image/jpeg, image/jpg"
+              onChange={e => onImageChange(e, 2)}
+            />
+            <input
+              type="file"
+              accept="image/jpeg, image/jpg"
+              onChange={e => onImageChange(e, 3)}
             />
           </div>
-          <input
-            className={s.inputL}
-            placeholder="Technique"
-            name="technique"
-            type="text"
-            value={this.state.technique}
-            onChange={this.handleInputChange}
-          />
-          <input
-            className={s.inputR}
-            placeholder="Description"
-            name="description"
-            type="text"
-            value={this.state.description}
-            onChange={this.handleInputChange}
-          />
-          <input
-            className={s.inputL}
-            placeholder="Hauteur (cm)"
-            name="height"
-            type="number"
-            value={this.state.height}
-            onChange={this.handleInputChange}
-          />
-          <input
-            className={s.inputR}
-            placeholder="Largeur (cm)"
-            name="width"
-            type="number"
-            value={this.state.width}
-            onChange={this.handleInputChange}
-          />
-          {this.isSculpture && (
-            <input
-              className={s.inputL}
-              placeholder="Longueur (cm)"
-              name="length"
-              type="number"
-              value={this.state.length}
-              onChange={this.handleInputChange}
-            />
-          )}
-          <input
-            type="file"
-            accept="image/jpeg, image/jpg"
-            onChange={e => this.handleImageChange(e, 0)}
-          />
-          {this.isSculpture && (
-            <div>
-              <input
-                type="file"
-                accept="image/jpeg, image/jpg"
-                onChange={e => this.handleImageChange(e, 1)}
+        )}
+        {imagePreviewUrls.map(
+          imagePreviewUrl =>
+            imagePreviewUrl !== '' && (
+              <img
+                key={imagePreviewUrl.toString()}
+                src={imagePreviewUrl}
+                alt="Sculpture de Marion Casters"
+                className={s.imagePreview}
               />
-              <input
-                type="file"
-                accept="image/jpeg, image/jpg"
-                onChange={e => this.handleImageChange(e, 2)}
-              />
-              <input
-                type="file"
-                accept="image/jpeg, image/jpg"
-                onChange={e => this.handleImageChange(e, 3)}
-              />
-            </div>
-          )}
-          {this.state.imagePreviewUrls.map(
-            imagePreviewUrl =>
-              imagePreviewUrl !== '' && (
-                <img
-                  key={imagePreviewUrl.toString()}
-                  src={imagePreviewUrl}
-                  alt="Sculpture de Marion Casters"
-                  className={s.imagePreview}
-                />
-              ),
-          )}
-          {canSubmit && <button type="submit">OK</button>}
-        </form>
-      </div>
-    );
-  }
+            ),
+        )}
+        {canSubmit && <button type="submit">OK</button>}
+      </form>
+    </div>
+  );
 }
 
 ItemAddForm.propTypes = {
