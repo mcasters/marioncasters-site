ad5a85f788dfc1d9e7672a1b2c7ba464f4d0198a
Unknown <yonk@wanadoo.fr>
2019-10-25 16:41:24 +0200

    Add close menu on click outside

--- src/components/LayoutRep/Navigation/CustomMobileNav/CustomMobileNav.js
+++ src/components/LayoutRep/Navigation/CustomMobileNav/CustomMobileNav.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import useStyles from 'isomorphic-style-loader/useStyles';
 
 import s from './CustomMobileNav.css';
@@ -12,43 +12,57 @@ import MenuButton from './MenuButton';
 function CustomMobileNav() {
   useStyles(s);
   const [menuOpen, setMenuOpen] = useState(false);
-
-  const handleMenuClick = () => {
-    setMenuOpen(!menuOpen);
-  };
+  const menuRef = useRef(null);
 
   const closeMenu = () => {
     setMenuOpen(false);
   };
 
+  const handleDocumentClick = e => {
+    if (!menuRef.current.contains(e.target) && menuOpen) closeMenu();
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleDocumentClick, false);
+    return function cleanup() {
+      document.removeEventListener('click', handleDocumentClick, false);
+    };
+  });
+
+  const handleMenuClick = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className={s.container}>
       <MenuButton open={menuOpen} onClick={handleMenuClick} />
-      <Menu open={menuOpen}>
-        <Link className={s.item} to={ROUTER.PRESENTATION} onClick={closeMenu}>
-          Présentation
-        </Link>
-        <Link className={s.item} to={ROUTER.PEINTURES} onClick={closeMenu}>
-          Peintures
-        </Link>
-        <Link className={s.item} to={ROUTER.SCULPTURES} onClick={closeMenu}>
-          Sculptures
-        </Link>
-        <Link className={s.item} to={ROUTER.DESSINS} onClick={closeMenu}>
-          Dessins
-        </Link>
-        <Link className={s.item} to={ROUTER.CONTACT} onClick={closeMenu}>
-          Contact
-        </Link>
-        <Link className={s.navHomeLink} to={ROUTER.HOME} onClick={closeMenu}>
-          <img
-            src={logoUrl}
-            srcSet={`${logoUrl2x} 2x`}
-            alt="Signature de Marion Casters"
-          />
-        </Link>
-        <p className={s.title}>Marion Casters</p>
-      </Menu>
+      <div ref={menuRef}>
+        <Menu open={menuOpen}>
+          <Link className={s.item} to={ROUTER.PRESENTATION} onClick={closeMenu}>
+            Présentation
+          </Link>
+          <Link className={s.item} to={ROUTER.PEINTURES} onClick={closeMenu}>
+            Peintures
+          </Link>
+          <Link className={s.item} to={ROUTER.SCULPTURES} onClick={closeMenu}>
+            Sculptures
+          </Link>
+          <Link className={s.item} to={ROUTER.DESSINS} onClick={closeMenu}>
+            Dessins
+          </Link>
+          <Link className={s.item} to={ROUTER.CONTACT} onClick={closeMenu}>
+            Contact
+          </Link>
+          <Link className={s.navHomeLink} to={ROUTER.HOME} onClick={closeMenu}>
+            <img
+              src={logoUrl}
+              srcSet={`${logoUrl2x} 2x`}
+              alt="Signature de Marion Casters"
+            />
+          </Link>
+          <p className={s.title}>Marion Casters</p>
+        </Menu>
+      </div>
     </div>
   );
 }
