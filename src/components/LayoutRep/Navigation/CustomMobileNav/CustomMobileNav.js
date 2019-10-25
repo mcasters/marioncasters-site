0152016f09a43d13bca41ef747ef127708ec0473
Unknown <yonk@wanadoo.fr>
2019-10-26 01:37:51 +0200

    Custom menu design

--- src/components/LayoutRep/Navigation/CustomMobileNav/CustomMobileNav.js
+++ src/components/LayoutRep/Navigation/CustomMobileNav/CustomMobileNav.js
@@ -1,10 +1,7 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useState } from 'react';
 import useStyles from 'isomorphic-style-loader/useStyles';
 
 import s from './CustomMobileNav.css';
-import Link from '../../../Link';
-import logoUrl from '../logo-45.png';
-import logoUrl2x from '../logo-100.png';
 import ROUTER from '../../../../constants/router';
 import Menu from './Menu';
 import MenuButton from './MenuButton';
@@ -12,57 +9,28 @@ import MenuButton from './MenuButton';
 function CustomMobileNav() {
   useStyles(s);
   const [menuOpen, setMenuOpen] = useState(false);
-  const menuRef = useRef(null);
 
   const closeMenu = () => {
     setMenuOpen(false);
   };
 
-  const handleDocumentClick = e => {
-    if (!menuRef.current.contains(e.target) && menuOpen) closeMenu();
-  };
-
-  useEffect(() => {
-    document.addEventListener('click', handleDocumentClick, false);
-    return function cleanup() {
-      document.removeEventListener('click', handleDocumentClick, false);
-    };
-  });
-
   const handleMenuClick = () => {
     setMenuOpen(!menuOpen);
   };
 
+  const routes = [
+    ['Présentation', `${ROUTER.PRESENTATION}`],
+    ['Peintures', `${ROUTER.PEINTURES}`],
+    ['Sculptures', `${ROUTER.SCULPTURES}`],
+    ['Dessins', `${ROUTER.DESSINS}`],
+    ['Contact', `${ROUTER.CONTACT}`],
+    ['Home', `${ROUTER.HOME}`],
+  ];
+
   return (
     <div className={s.container}>
       <MenuButton open={menuOpen} onClick={handleMenuClick} />
-      <div ref={menuRef}>
-        <Menu open={menuOpen}>
-          <Link className={s.item} to={ROUTER.PRESENTATION} onClick={closeMenu}>
-            Présentation
-          </Link>
-          <Link className={s.item} to={ROUTER.PEINTURES} onClick={closeMenu}>
-            Peintures
-          </Link>
-          <Link className={s.item} to={ROUTER.SCULPTURES} onClick={closeMenu}>
-            Sculptures
-          </Link>
-          <Link className={s.item} to={ROUTER.DESSINS} onClick={closeMenu}>
-            Dessins
-          </Link>
-          <Link className={s.item} to={ROUTER.CONTACT} onClick={closeMenu}>
-            Contact
-          </Link>
-          <Link className={s.navHomeLink} to={ROUTER.HOME} onClick={closeMenu}>
-            <img
-              src={logoUrl}
-              srcSet={`${logoUrl2x} 2x`}
-              alt="Signature de Marion Casters"
-            />
-          </Link>
-          <p className={s.title}>Marion Casters</p>
-        </Menu>
-      </div>
+      <Menu open={menuOpen} onClick={closeMenu} routes={routes} />
     </div>
   );
 }
--- src/components/LayoutRep/Navigation/CustomMobileNav/Menu.js
+++ src/components/LayoutRep/Navigation/CustomMobileNav/Menu.js
@@ -1,22 +1,65 @@
-import React from 'react';
+/* eslint-disable css-modules/no-undef-class */
+import React, { useEffect, useRef } from 'react';
 import useStyles from 'isomorphic-style-loader/useStyles';
 import PropTypes from 'prop-types';
 
 import s from './Menu.css';
+import Link from '../../../Link';
+import logoUrl from '../logo-45.png';
+import logoUrl2x from '../logo-100.png';
 
-function Menu({ open, children }) {
+function Menu({ open, onClick, routes }) {
   useStyles(s);
+  const menuRef = useRef(null);
+
+  const handleDocumentClick = e => {
+    if (open && !menuRef.current.contains(e.target)) onClick();
+  };
+
+  useEffect(() => {
+    if (open) {
+      document.addEventListener('click', handleDocumentClick, false);
+      return function cleanup() {
+        document.removeEventListener('click', handleDocumentClick, false);
+      };
+    }
+    return undefined;
+  });
 
   return (
-    <div className={open ? `${s.container} ${s.open}` : `${s.container}`}>
-      {open && <div className={s.menuList}>{children}</div>}
+    <div
+      ref={menuRef}
+      className={open ? `${s.container} ${s.open}` : `${s.container}`}
+    >
+      <nav className={s.menuList}>
+        {routes.map(route => {
+          if (route[0] === 'Home') {
+            return (
+              <Link className={s.navHomeLink} to={route[1]} onClick={onClick}>
+                <img
+                  src={logoUrl}
+                  srcSet={`${logoUrl2x} 2x`}
+                  alt="Signature de Marion Casters"
+                />
+              </Link>
+            );
+          }
+          return (
+            <Link className={s.item} to={route[1]} onClick={onClick}>
+              {route[0]}
+            </Link>
+          );
+        })}
+        <p className={s.name}>Marion Casters</p>
+      </nav>
     </div>
   );
 }
 
 Menu.propTypes = {
   open: PropTypes.bool.isRequired,
-  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func.isRequired,
+  routes: PropTypes.array.isRequired,
 };
 
 export default Menu;
